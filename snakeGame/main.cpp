9cefc86032cb42ef85eda5977c93d3b8a8564b26
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-08-27 12:32:13 +0530

    added fix to display

--- snakeGame/main.cpp
+++ snakeGame/main.cpp
@@ -1,84 +1,114 @@
-#include <iostream>
-
+#include<iostream>
+#include<stdio.h>
 using namespace std;
-
-int starting_head=0;
-int START_SNAKE_LENGTH=4;
-
-
-struct Snake{
-char x;
-char y;
-char data;
-Snake *next;
-};
-Snake * head_x = NULL;
-Snake *head_y = NULL;
-
-int INSERT_HORIZONTAL(char value='*')
+int startingPoint = 0;
+int endingPoint = 4;
+struct Snake
 {
-    Snake *newNode  = new Snake;
+    char x;
+    char y;
+    char data;
+    Snake *next;
+};
+
+Snake *head = NULL;
+
+
+char insertSnake(char value)
+{
+    Snake *newNode = new Snake;
     newNode->data = value;
 
-    if(head_x == NULL)
+    if(head != NULL)
     {
-        head_x = newNode;
         newNode->next = NULL;
+        head = newNode;
     }
     else
     {
-        newNode->next = head_x;
-        head_x = newNode;
+        newNode->next = head;
+        head = newNode;
     }
 
     return newNode->data;
-    newNode->next;
 }
 
-/* CREATED THE BOX OUTLINE */
-void createBox(char gameBoundary[][80])
-{
-int i , j;
-int row = 10;
-int col = 40;
+/* DEFINITION OF THE MATRIX FUNCTION */
+void CreateBoundary(char matrix[][80]);
+/* END OF DEFINITION OF THE MATRIX FUNCTION */
 
-    for(i = 0 ; i < 20 ; i++)
+/* DEFINITION OF THE DISPLAY FUNCTION OF THE MATRIX */
+void DisplayBoundary(char matrix[][80]);
+/* END OF DEFINITION OF THE MATRIX FUNCTION */
+
+
+/* DECLARATION OF THE ADD FUNCTION , WHERE THE SNAKE IS ADDED TO HEAD */
+
+void addSnake(char matrix[][80])
+{
+    int row = 10;
+    int col = 40;
+    while(startingPoint < endingPoint)
     {
-        for(j = 0 ; j < 80 ; j++)
+        matrix[row][col++] = insertSnake('*');
+        startingPoint++;
+    }
+}
+
+/* END OF THE ADD FUNCTION , WHERE THE SNAKE IS ADDED */
+
+
+/* THE MAIN FUNCTION WHERE ALL THE FUNCTIONS OF DISPLAY,MOVE,EAT ARE CALLED */
+int main()
+{
+    char matrix[20][80];
+
+    CreateBoundary(matrix); //creating the boundary;
+    addSnake(matrix);
+    DisplayBoundary(matrix);
+
+    return 0;
+}
+/* END OF THE MAIN FUNCTION BODY */
+
+
+/* DEFINITION */
+void CreateBoundary(char matrix[][80])
+{
+   int i, j;
+   int row = 10;
+   int col = 40;
+
+    for (i = 1; i <= 20; i++)
+    {
+        for (j = 1; j <= 80; j++)
         {
 
-            gameBoundary[i][j] = '#';
-             gameBoundary[5][5]=INSERT_HORIZONTAL('*');
-            if( (i>0 && i<19)  && (j>0 && j<79) )
+            if (i==1 || i==20 || j==1 || j==80)
             {
-                gameBoundary[row][col] = INSERT_HORIZONTAL('*');
-                // gameBoundary[i][j] =' ';
-
-               cout<<" ";
+                matrix[i][j] ='#';
             }
             else
             {
-
-                cout<<gameBoundary[i][j];
+                matrix[i][j] =' ';
             }
         }
-        cout<<"\n";
 
-
+        printf("\n");
     }
 
-
-}
-/* BOX OUTLINE ENDS */
-
-int main()
-{
-    char gameBoundary[20][80];
-    createBox(gameBoundary);
-
-
-
 }
 
-
-
+/* DISPLAYING THE BOUNDARY */
+void DisplayBoundary(char matrix[][80])
+{int i , j;
+     for (i = 1; i <= 20; i++)
+    {
+        for (j = 1; j <= 80; j++)
+        {
+          cout<<matrix[i][j];
+        }
+        cout<<'\n';
+    }
+}
+/* DISPLAY FINISHED */
